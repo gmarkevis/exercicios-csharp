2837954ef031af00f5d3cde3b855d44e53a7df4f
Guilherme Pinheiro <guilhermemarkevis@hotmail.com>
2020-12-20 19:05:13 -0300

    Criação projeto do exemplo 1 sobre enumerações

---
+++ Mod9-Enumeracao1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mod9_Enumeracao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
